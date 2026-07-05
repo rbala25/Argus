c90291308f3de425ced19f47ea80888f2fcf4c49
rbala25 <rishibala2007@gmail.com>
2026-07-04 23:59:41 -0400

    interface file definitions

---
+++ Argus.srcs/sources_1/new/byte_stream.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/04/2026 11:58:58 PM
+// Design Name: 
+// Module Name: byte_stream
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module byte_stream(
+
+    );
+endmodule
