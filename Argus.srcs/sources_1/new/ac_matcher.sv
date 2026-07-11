db44347a8c3bc4d173415cab28b8172405b1055b
rbala25 <rishibala2007@gmail.com>
2026-07-10 22:47:25 -0400

    ac matcher fsm detailing

--- Argus.srcs/sources_1/new/ac_matcher.sv
+++ Argus.srcs/sources_1/new/ac_matcher.sv
@@ -33,5 +33,23 @@ module ac_matcher #(
   match_out.dut_mp mout
 );
 
+  typedef enum logic {S_IDLE, S_LOOKUP} state_e;
+  state_e fsm;
+ 
+  logic [STATE_W-1:0] current_state;
+  logic [7:0] in_byte;
+  logic [STATE_W-1:0] next_state;
+ 
+  logic [MATCH_ID_W-1:0] match_rom [NUM_STATES]; //from match.mem
+  initial $readmemh(MATCH_FILE, match_rom);
+
+  assign next_state = mem_bus.row_data[in_byte*STATE_W +: STATE_W]; //256 transitions for one state
+ 
+  assign stream.ready = (fsm == S_IDLE); //only when idle
+  assign mem_bus.req = (fsm == S_LOOKUP);
+  assign mem_bus.state_addr = current_state;
+ 
+  assign mout.current_state = current_state;
+
 
 endmodule
