cfab9f49ccd8cf2079586241f92834aecf0fa855
rbala25 <rishibala2007@gmail.com>
2026-07-31 23:16:49 -0400

    slow mem logic

--- Argus.srcs/sources_1/new/slow_mem.sv
+++ Argus.srcs/sources_1/new/slow_mem.sv
@@ -31,6 +31,24 @@ module slow_mem #( //stand in for ddr3
   mem.mem_mp mem_bus
 );
 
+localparam int row_w = state_w * 256;
 
+logic [state_w-1:0] trans_flat [0:num_states*256-1];
+initial $readmemh(mem_file, trans_flat);
+
+function automatic logic [row_w-1:0] build_row(input logic [state_w-1:0] s); //single row
+logic [row_w-1:0] r;
+for (int b = 0; b < 256; b++) begin
+  r[b*state_w +: state_w] = trans_flat[int'(s)*256 + b];
+end
+return r;
+endfunction
+
+typedef enum logic [1:0] {s_idle, s_wait, s_done} state_t;
+state_t fsm;
+
+logic [$clog2(latency+1)-1:0] cnt;
+logic [state_w-1:0] addr_q;
+logic [row_w-1:0] row_q;
 
 endmodule
