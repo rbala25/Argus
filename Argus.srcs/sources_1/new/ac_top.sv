b5a7c39da4f879c8fda6dfcc5e05e156ffe1daeb
rbala25 <rishibala2007@gmail.com>
2026-08-03 14:50:32 -0400

    updated curr argus top

--- Argus.srcs/sources_1/new/ac_top.sv
+++ Argus.srcs/sources_1/new/ac_top.sv
@@ -20,26 +20,65 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module argus_top(
-  input logic clk,
-  input logic rst_n
+module argus_top #(
+//  input logic clk,
+//  input logic rst_n
+//);
+
+//byte_stream stream_if(clk, rst_n);
+//mem mem_if(clk, rst_n);
+//match_out match_if(clk, rst_n);
+
+//ac_matcher matcher(
+//.clk(clk),
+//.rst_n(rst_n),
+//.stream(stream_if),
+//.mem_bus(mem_if),
+//.mout(match_if)
+//);
+
+//slow_mem model(
+//.clk(clk),
+//.rst_n(rst_n),
+//.mem_bus(mem_if)
+//);
+    parameter int state_w = 6,
+    parameter int nLines = 8,
+    parameter int mem_lat = 20,
+    parameter string trans_mem = ""
+) (
+    input logic clk,
+    input logic rst,
+    byte_stream.src_mp bs,
+    match_out.src_mp match,
+    output logic [31:0] hits,
+    output logic [31:0] misses
 );
 
-byte_stream stream_if(clk, rst_n);
-mem mem_if(clk, rst_n);
-match_out match_if(clk, rst_n);
+mem i_mc();
+mem i_cs();
 
-ac_matcher matcher(
-.clk(clk),
-.rst_n(rst_n),
-.stream(stream_if),
-.mem_bus(mem_if),
-.mout(match_if)
+ac_matcher #(.STATE_W(state_w)) u_matcher (
+    .clk(clk), .rst(rst),
+    .bs(bs),
+    .mem_if(i_mc.mem_mp),
+    .match(match)
 );
 
-slow_mem model(
-.clk(clk),
-.rst_n(rst_n),
-.mem_bus(mem_if)
+cache #(.state_w(state_w), .nLines(nLines)) u_cache (
+    .clk(clk), .rst(rst),
+    .from_matcher(i_mc.cache_mp),
+    .to_sm(i_cs.mem_mp),
+    .hits(hits),
+    .misses(misses)
+);
+
+slow_mem #(
+    .STATE_W(state_w),
+    .MEM_LATENCY(mem_lat),
+    .TRANS_MEM(trans_mem)
+) u_slow_mem (
+    .clk(clk), .rst(rst),
+    .mem_if(i_cs.cache_mp)
 );
 endmodule
